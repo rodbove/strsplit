a0f845a83160685cbc3c14a083003c1ba3ef829f
Rodrigo Mello <rodbove@gmail.com>
2023-10-16 08:52:13 -0300

    refactor: add different lifetimes

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,13 @@
 // #![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
 
 #[derive(Debug)]
-pub struct StrSplit<'a> {
-    remainder: Option<&'a str>,
-    delimiter: &'a str,
+pub struct StrSplit<'haystack, 'delimiter> {
+    remainder: Option<&'haystack str>,
+    delimiter: &'delimiter str,
 }
 
-impl<'a> StrSplit<'a> {
-    pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
+impl<'haystack, 'delimiter> StrSplit<'haystack, 'delimiter> {
+    pub fn new(haystack: &'haystack str, delimiter: &'delimiter str) -> Self {
         Self {
             remainder: Some(haystack),
             delimiter,
@@ -16,8 +16,8 @@ impl<'a> StrSplit<'a> {
     }
 }
 
-impl<'a> Iterator for StrSplit<'a> {
-    type Item = &'a str;
+impl<'haystack, 'delimiter> Iterator for StrSplit<'haystack, 'delimiter> {
+    type Item = &'haystack str;
     fn next(&mut self) -> Option<Self::Item> {
         let remainder = self.remainder.as_mut()?;
         if let Some(next_delim) = remainder.find(self.delimiter) {
@@ -32,16 +32,16 @@ impl<'a> Iterator for StrSplit<'a> {
     }
 }
 
-// fn until_char<'s>(s: &'s str, c: char) -> &'s str {
-//     StrSplit::new(s, &format!("{}", c))
-//         .next()
-//         .expect("StrSplit always gives at least one result")
-// }
+fn until_char<'s>(s: &'s str, c: char) -> &'s str {
+    StrSplit::new(s, &format!("{}", c))
+        .next()
+        .expect("StrSplit always gives at least one result")
+}
 
-// #[test]
-// fn until_char_test() {
-//     assert_eq!(until_char("hello world", 'o'), "hell")
-// }
+#[test]
+fn until_char_test() {
+    assert_eq!(until_char("hello world", 'o'), "hell")
+}
 
 #[test]
 fn split_test() {
